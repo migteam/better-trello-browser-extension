af3988aacd6388a899ec0e08db31287efbedd01a
Seweryn Kalemba <sewen.kal@gmail.com>
2024-08-23 23:36:47 +0200

    feat: add footer

--- action/popup.tsx
+++ action/popup.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useStorageSyncQuery } from "../queries/storage";
 import { Switch } from "../components/switch";
 
+import manifest from "../manifest.json";
+
 let browser: typeof chrome;
 
 function Popup() {
@@ -30,59 +32,72 @@ function Popup() {
         </h1>
       </header>
 
-      <fieldset className="flex flex-col gap-2 p-3">
-        <div className="flex items-center justify-between">
-          <label className="dark:text-white text-sm leading-none pr-2">
-            Enlargen Card View
-          </label>
+      <div className="p-3">
+        <fieldset className="flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <label className="dark:text-white text-sm leading-none pr-2">
+              Enlargen Card View
+            </label>
 
-          <Switch
-            defaultChecked={storageSyncQuery.data?.largeCardBack}
-            onCheckedChange={(checked) => {
-              browser.storage.sync.set({ largeCardBack: checked });
-            }}
-          />
-        </div>
+            <Switch
+              defaultChecked={storageSyncQuery.data?.largeCardBack}
+              onCheckedChange={(checked) => {
+                browser.storage.sync.set({ largeCardBack: checked });
+              }}
+            />
+          </div>
 
-        <div className="flex items-center justify-between">
-          <label className="dark:text-white text-sm leading-none pr-2">
-            Use legacy markdown editor
-          </label>
+          <div className="flex items-center justify-between">
+            <label className="dark:text-white text-sm leading-none pr-2">
+              Use legacy markdown editor
+            </label>
 
-          <Switch
-            defaultChecked={storageSyncQuery.data?.useLegacyMarkdownEditor}
-            onCheckedChange={(checked) => {
-              browser.storage.sync.set({ useLegacyMarkdownEditor: checked });
-            }}
-          />
-        </div>
+            <Switch
+              defaultChecked={storageSyncQuery.data?.useLegacyMarkdownEditor}
+              onCheckedChange={(checked) => {
+                browser.storage.sync.set({ useLegacyMarkdownEditor: checked });
+              }}
+            />
+          </div>
 
-        <div className="flex items-center justify-between">
-          <label className="dark:text-white text-sm leading-none pr-2">
-            Auto hide top bar
-          </label>
+          <div className="flex items-center justify-between">
+            <label className="dark:text-white text-sm leading-none pr-2">
+              Auto hide top bar
+            </label>
 
-          <Switch
-            defaultChecked={storageSyncQuery.data?.autoHideTopBar}
-            onCheckedChange={(checked) => {
-              browser.storage.sync.set({ autoHideTopBar: checked });
-            }}
-          />
-        </div>
+            <Switch
+              defaultChecked={storageSyncQuery.data?.autoHideTopBar}
+              onCheckedChange={(checked) => {
+                browser.storage.sync.set({ autoHideTopBar: checked });
+              }}
+            />
+          </div>
 
-        <div className="flex items-center justify-between">
-          <label className="dark:text-white text-sm leading-none pr-2">
-            Show Card Id
-          </label>
+          <div className="flex items-center justify-between">
+            <label className="dark:text-white text-sm leading-none pr-2">
+              Show Card Id
+            </label>
 
-          <Switch
-            defaultChecked={storageSyncQuery.data?.showCardId}
-            onCheckedChange={(checked) => {
-              browser.storage.sync.set({ showCardId: checked });
-            }}
-          />
-        </div>
-      </fieldset>
+            <Switch
+              defaultChecked={storageSyncQuery.data?.showCardId}
+              onCheckedChange={(checked) => {
+                browser.storage.sync.set({ showCardId: checked });
+              }}
+            />
+          </div>
+        </fieldset>
+
+        <small className="flex-row text-xs flex gap-1 mt-3 dark:text-white/40 text-black/40">
+          <strong>Version {manifest.version}</strong>
+          <span>·</span>
+          <a
+            href="https://github.com/migteam/better-trello-browser-extension/issues/new"
+            className="hover:underline underline-offset-4"
+          >
+            🐛 Report an issue
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
